a82a85aafad9db89e15bc070f9e8c44afedfb5f0
Kamil Koziol <koziol.kamil@protonmail.com>
2025-07-08 20:16:09 +0200

    add battery state

--- config.h
+++ config.h
@@ -66,7 +66,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { battery_perc, " %s%% | ",           "BAT0" },
+    { battery_perc, " %s%%",           "BAT0" },
+    { battery_state, " %s | ",               "BAT0" },
     { wifi_essid, "W: %s | ",           "wlan0" },
     { datetime, "%s",           "%F %T" },
 };
